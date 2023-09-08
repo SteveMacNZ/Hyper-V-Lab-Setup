d3cf7b19021d91860024e392456af0a302e38a85
SteveMacNZ <54332937+SteveMacNZ@users.noreply.github.com>
2023-09-08 12:43:34 +1200

    Update for MSLAB Supplemental

---
+++ MSLAB-Supplmental/LabConfig/LabConfig.ps1
@@ -0,0 +1,48 @@
+#^ Lab Configuration Settings
+$LabConfig=@{
+    DomainAdminName="LabAdmin";                     # Used during 2_CreateParentDisks (no affect if changed after this step)
+    AdminPassword="LS1setup!";                      # Used during 2_CreateParentDisks. If changed after, it will break the functionality of 3_Deploy.ps1
+    Prefix = "MSLab-";                              # (Optional) All VMs and vSwitch are created with this prefix, so you can identify the lab. If not specified, Lab folder name is used
+    SwitchName = "vSWI_Lab";                        # (Optional) Name of vSwitch
+    SecureBoot=$true;                               # (Optional) Useful when testing unsigned builds (Useful for MS developers for daily builds)
+    DCEdition="4";                                  # 4 for DataCenter or 3 for DataCenterCore
+    ServerISOFolder="$PSScriptRoot\_ISOs";          # (Optional) Path to Server ISO images
+    ServerMSUsFolder="$PSScriptRoot\_MSUs";         # (Optional) Path to Server CU updates
+    DCVMProcessorCount=2;                           # (Optional) Sets DC vCPU count (default is 2vCPUs)
+    EnableGuestServiceInterface=$true;              # (Optional) If True, then Guest Services integration component will be enabled on all VMs. This allows simple file copy from host to guests.
+    InstallSCVMM="No";                              # (Optional) Yes/Prereqs/SQL/ADK/No
+    DomainNetbiosName="Corp";                       # (Optional) If set, custom domain NetBios name will be used. if not specified, Default "corp" will be used
+    DomainName="Corp.contoso.com";                  # (Optional) If set, custom DomainName will be used. If not specified, Default "Corp.contoso.com" will be used
+    DefaultOUName="Managed Objects";                # (Optional) If set, custom OU for all machines and account will be used. If not specified, default "Workshop" is created
+    Internet=$true;                                 # (Optional) If $true, it will add external vSwitch and configure NAT in DC to provide internet (Logic explained below)
+    CustomDnsForwarders=@("8.8.8.8","1.1.1.1");     # (Optional) If configured, script will use those servers as DNS fordwarders in DC (Defaults to 8.8.8.8 and 1.1.1.1)
+    PullServerDC=$true;                             # (Optional) If $false, then DSC Pull Server will not be configured on DC
+    DHCPscope="10.10.10.0";                         # (Optional) 10.0.0.0 is configured if nothing is specified. Scope has to end with .0 (like 10.10.10.0). It's always /24       
+    TelemetryLevel="Full";                          # (Optional) If configured, script will stop prompting you for telemetry. Values are "None","Basic","Full"
+    AutoStartAfterDeploy=$false;                    # (Optional) If $false, no VM will be started; if $true or 'All' all lab VMs will be started after Deploy script; if 'DeployedOnly' only newly created VMs will be started.
+    AutoCleanUp=$false;                             # (Optional) If set, after creating initial parent disks, files that are no longer necessary will be cleaned up. Best suited for use in automated deployments.
+    AdditionalNetworksConfig=@();                   # Just empty array for config below
+    VMs=@()                                         # Just empty array for config below
+}
+
+#^ VM Creation
+#& Create 2x VMs for Azure AD Connect
+1..2 | ForEach-Object { 
+    $VMNames="SVR-AADC";                         # Here you can bulk edit name of 2 VMs created. In this case will be SVR-AADC1,SVR-AADC2 created
+    $LABConfig.VMs += @{
+        VMName = "$VMNames$_" ;
+        Configuration = 'Simple' ;               # Simple/S2D/Shared/Replica
+        ParentVHD = 'Win2022_G2.vhdx';           # VHD Name from .\ParentDisks folder
+        VMProcessorCount=2;                      # Set VMs to 2 VCPUs
+        MemoryStartupBytes= 1GB;                 # Startup memory size
+        vTPM=$true;                              # (Optional) if $true, vTPM will be enabled for virtual machine. Gen2 only.
+        Generation=2;                            # (Optional) set VM generation to 2
+        DisableWCF=$true;                        # (Optional) If $True, then Disable Windows Consumer Features registry is added= no consumer apps in start menu.
+        AddToolsVHD=$True                        # Add Tools VHD to VM
+    }
+}
+
+#& Management LabVM
+$LABConfig.VMs += @{ VMName = 'SVR-MGT'; Configuration = 'Simple'; ParentVHD = 'Win2022_G2.vhdx'; MemoryStartupBytes= 1GB ; vTPM=$true ; DisableWCF=$true ; Generation=2 ; VMProcessorCount=2 ; AddToolsVHD=$True }
+#& Windows Admin Centre LabVM
+$LABConfig.VMs += @{ VMName = 'SVR-WAC'; Configuration = 'Simple'; ParentVHD = 'Win2022_G2.vhdx'; MemoryStartupBytes= 1GB ; vTPM=$true ; DisableWCF=$true ; Generation=2 ; VMProcessorCount=2 ; AddToolsVHD=$True }
--- README.md
+++ README.md
@@ -1,31 +1,21 @@
-# Hyper-V Lab Setup
+# Hyper-V Lab Setup - Updated to leverage MSLAB
 ## Overview
- PowerShell scripts for creation of lab VMs in Hyper-V, it is not designed for Production use
+ Additional files and PowerShell scripts to be used in conjuntction with the MSLAB kit https://aka.ms/mslab/download
 
-## Quick Lab Setup
- Not designed to run as a script - contains the commands for;
- * VM creation
- * Quick configuration of VMs once logged into Windows
-   * Computer Name
-   * Network Configuration
-   * Disk Configutation
-   * Domain Controller VM
-     * Installs required roles and features
-     * Installs new Active Directory Forest
-     * Installs and configures DHCP
-     * Creates base OUs and Security Groups
-     * Creates User accounts
-   * Management Server VM and joins to Domain
-   * Windows 10 VM and joins to Domain
+## MSLAB-Supplmental
+ Contains customisations and additional configuration scripts to be used with MSLAB:
+ * DesktopInfo 
+   * desktopinfo.ini ==> customised configuration file for DesktopInfo
+   * Fujitsu-Logo-transparent.png ==> logo used in configuration file  
+ * LabConfig
+   * LabConfig.ps1 ==> replaces the default LabConfig.ps1 provided in the MSLAB kit (rename orginal before replacing)
+ * PostSetup - contains scripts to be run post lab configuration
+   * What?
 
-## Full Lab Setup
- Expands on Quick Lab setup to also include PKI and NDES servers
- - [ ] To be completed
+
+
+
 
 ## Notes
  * Best viewed in Microsoft Visual Studio Code with Colorful Comments extension installed
- * Requires Hyper-V Role installed on your device (Works on Windows 10 - Enable the Hyper-V feature NB: Virtualisation needs to be enabled in BIOS)
- * To create parent VHDX - Create a new Hyper-V VM and install the OS. At the OOBE screen (Entering of Country) press Ctrl + Shift + F3 to enter Audit Mode
- * In Audit Mode install any Windows Updates and/or additional apps you require (reboot as required) when finished editing run sysprep with Enter OOBE | Generalize | Shutdown - once shut down copy to the folder path e.g. C:\Hyper-V\Virtual Hard Disks and rename as required. Delete the parent VM, You can now use this VHDX as the parent disk(s) for your VMs... 
- * If you require a isolated network create a private switch in Hyper-V and use pfsense VM to provide firewall / routing.  
- * New-VMSwitch -Name "vSWI_172.70.70.x" -SwitchType "Private" -Notes "vSwitch for 172.70.70.0/24 Subnet"
+ * Requires Hyper-V Role installed on your device (Works on Windows 10/11 - Enable the Hyper-V feature NB: Virtualisation needs to be enabled in BIOS)
